8adadcbd460c5700a3a45d0139eee841c56d4b1f
ro-ix-y <78548891+ro-ix-y@users.noreply.github.com>
2021-02-06 00:26:36 +0600

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.util.regex.*;
+public class Solution { 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		int start = sc.nextInt();
+		int end = sc.nextInt();
+		System.out.println(s.substring(start,end));
+		
+		
+	}
+
+}
---
+++ Static.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+import java.util.regex.*;
+public class Static {
+	static Scanner  sc = new Scanner(System.in);
+	static int H = sc.nextInt();
+	static int B = sc.nextInt();
+	static boolean flag = true;
+	static {
+		try
+		{
+			if(B<=0 || H<=0)
+			{
+				flag =false;
+				throw new Exception ("Breadth and height must be positive");
+			}
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+public static void main(String[] args) {
+	if(flag) {
+		int area = B*H;
+		System.out.println(area);
+	}
+}
+	
+
+}
